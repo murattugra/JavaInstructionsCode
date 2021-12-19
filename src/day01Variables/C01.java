26c503ef1aeff642f333798ccfc266ecffb02565
Murat Arslan <mrterkmglsv@gmail.com>
2021-12-19 20:03:07 +0100

    variables

---
+++ src/day01Variables/C01.java
@@ -0,0 +1,28 @@
+package day01Variables;
+
+public class C01 {
+
+	public static void main(String[] args) {
+		
+		String ogrenciIsim = "Celal";
+		
+		System.out.println(ogrenciIsim);
+		
+		
+		int sayi;
+		sayi=10;
+		
+		System.out.println(sayi);
+		
+		sayi=25; 
+	
+		System.out.println(sayi);
+
+		ogrenciIsim="Mustafa";
+		
+		System.out.println(ogrenciIsim);
+		
+		
+	}
+
+}
